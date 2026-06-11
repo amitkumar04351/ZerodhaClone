fb343a74aa4eab4631e831bf88647939099402a9
amitkumar04351 <kumarssuraj01012002@gmail.com>
2026-06-12 03:41:27 +0530

    Update HoldingsModel to use plural collection name

--- Backend/model/HoldingsModel.js
+++ Backend/model/HoldingsModel.js
@@ -2,6 +2,6 @@ const { model } = require("mongoose");
 
 const { HoldingsSchema } = require("../schemas/HoldingsSchema");
 
-const HoldingsModel = new model("holding", HoldingsSchema);
+const HoldingsModel = new model("Holding", HoldingsSchema,"holdings");
 
 module.exports = { HoldingsModel };
