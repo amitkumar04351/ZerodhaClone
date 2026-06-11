0f03bacb214fc2c7ba19fdb8cff770f5f0cfb7a1
amitkumar04351 <kumarssuraj01012002@gmail.com>
2026-06-11 14:30:22 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,8 +14,9 @@ const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Zerodha Clone Backend is Running ");
+app.get("/holdings", async (req, res) => {
+  const holdings = await Holding.find();
+  res.json(holdings);
 });
 
 app.use(cors({
